0fd3c42dc86ca5fb0fdabad57064e88508cb3b18
TylrPopcorn <tyler.mcmillan99@yahoo.com>
2024-09-26 11:42:56 -0400

    header

---
+++ src/Components/Header.svelte
@@ -0,0 +1,21 @@
+
+<!-- HTML -->
+<header>
+   <img src="../assets/svelte-horizontal.png" alt="">
+    <h1>The Polls</h1>
+</header>
+
+<!-- CSS -->
+<style>
+    header {
+     background: #f7f7f7;
+     padding: 20px;
+    }
+    h1{
+        text-align: center;
+        margin: 0;
+    }
+    img{
+        max-width: 180px;
+    }
+</style>
